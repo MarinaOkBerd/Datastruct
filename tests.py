038595b41e1a8d7c70ab9cff3bcc9bf1231edaac
Marina Berdnikova <berdni4ka@rambler.ru>
2023-03-12 00:16:45 +0300

    Add tests

--- tests.py
+++ tests.py
@@ -1,5 +1,7 @@
 import unittest
 from node import Node, Stack
+from custom_queue import Queue
+from linked_list import LinkedList
 
 
 class TestNode(unittest.TestCase):
@@ -23,5 +25,27 @@ class TestStack(unittest.TestCase):
         self.assertEqual(stack.top.data, 'data2')
         self.assertEqual(stack.pop(), 'data2')
 
+
+class TestQueue(unittest.TestCase):
+    def test_Queue(self):
+        queue = Queue()
+        queue.enqueue('data1')
+        queue.enqueue('data2')
+        queue.enqueue('data3')
+        self.assertEqual(queue.head.data, 'data1')
+        self.assertEqual(queue.dequeue(), 'data1')
+        self.assertEqual(queue.dequeue(), 'data2')
+        self.assertEqual(queue.dequeue(), 'data3')
+        self.assertEqual(queue.dequeue(), None)
+
+
+class TestLinkedList(unittest.TestCase):
+    def test_LinkedList(self):
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1})
+        ll.insert_at_end({'id': 2})
+        ll.insert_at_end({'id': 3})
+        ll.insert_beginning({'id': 0})
+        self.assertEqual(ll.print_ll(), "{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None")
 
 
